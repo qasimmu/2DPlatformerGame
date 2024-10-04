6a1efdd817bf0581c66019a34cd6f37b8b922527
Qasim Obaidullah <24843273@stu.mmu.ac.uk>
2024-10-05 00:02:10 +0100

    Update 2-0

---
+++ Assets/_Scripts/GameManager.cs
@@ -0,0 +1,102 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class GameManager : MonoBehaviour
+{
+
+    public GameObject levelPausePanel, levelCompletePanel, levelFailedPanel;
+    public string NextScene;
+    public string PreviousScene;
+
+    [Header("Loading Screen")]
+    public Image LoadingBar;
+    public Text loading_Text;
+    public GameObject loadingPanel;
+    public float loadingDelayTime = 2f; 
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void PauseGame(){
+
+    levelPausePanel.SetActive(true);
+
+    }
+        public void ResumeGame(){
+
+    levelPausePanel.SetActive(false);
+
+    }
+    public void RestartGame(){
+
+            StartCoroutine(LoadSceneWithDelay(NextScene));
+
+    }
+    public void LevelCompleted(){
+
+    }
+    public void LevelFailed(){
+
+    }
+    public void NextLevel(){
+
+    }
+
+    public void HomeGame(){
+            StartCoroutine(LoadSceneWithDelay(PreviousScene));
+
+    }
+    public void OpenGithubLink()
+    {
+
+        Application.OpenURL("https://github.com/qasimmu/2DPlatformerGame.git");
+    }
+    
+        public void OpenItchio()
+    {
+
+        Application.OpenURL("https://rainbow-flamingo.itch.io/jumpquest");
+    }
+
+        void UpdateLoadingUI(float progress)
+    {
+        if (LoadingBar) LoadingBar.fillAmount = progress;
+        if (loading_Text) loading_Text.text = Mathf.RoundToInt(progress * 100) + "%";
+    }
+    IEnumerator LoadSceneWithDelay(string name)
+    {
+if(loadingDelayTime>0){
+        loadingPanel.SetActive(true);
+
+}
+        float timer = 0f;
+
+        AsyncOperation asyncOperation = SceneManager.LoadSceneAsync(name);
+        asyncOperation.allowSceneActivation = false;
+        while (timer < loadingDelayTime)
+        {
+            timer += Time.deltaTime;
+            float fillAmount = Mathf.Clamp01(timer / loadingDelayTime);
+            UpdateLoadingUI(fillAmount);
+            yield return null;
+        }
+        yield return new WaitForSeconds(1f);
+
+        asyncOperation.allowSceneActivation = true;
+
+    }
+
+}
---
+++ Assets/_Scripts/MainMenu_Manager.cs
@@ -0,0 +1,110 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+
+public class MainMenu_Manager : MonoBehaviour
+{
+
+    public static MainMenu_Manager instance;
+
+    public string NextScene;
+    public string PreviousScene;
+
+    [Header("Loading Screen")]
+    public Image LoadingBar;
+    public Text loading_Text;
+    public GameObject loadingPanel;
+    public float loadingDelayTime = 2f; 
+
+
+    private void Awake()
+    {
+        if (instance == null)
+        {
+            instance = this;
+        }
+
+    }
+
+    void Start()
+    {
+
+        Cursor.lockState = CursorLockMode.None;
+        Cursor.visible = true;
+
+        OnStartInitialize();
+
+    }
+
+    public void OnStartInitialize()
+    {
+        Time.timeScale = 1f;
+
+        if (SoundManager._SoundManager)
+        {
+            SoundManager._SoundManager.playMainMenuSounds(0.8f);
+
+        }
+
+    }
+
+    public void PlayButtonClickSound()
+    {
+        if (SoundManager._SoundManager)
+        {
+            SoundManager._SoundManager.playButtonClickSound();
+
+        }
+    }
+
+    public void Play()
+    {
+
+        StartCoroutine(LoadSceneWithDelay(NextScene));
+    }
+
+    void UpdateLoadingUI(float progress)
+    {
+        if (LoadingBar) LoadingBar.fillAmount = progress;
+        if (loading_Text) loading_Text.text = Mathf.RoundToInt(progress * 100) + "%";
+    }
+    IEnumerator LoadSceneWithDelay(string name)
+    {
+
+if(loadingDelayTime>0){
+        loadingPanel.SetActive(true);
+
+}
+        float timer = 0f;
+
+        AsyncOperation asyncOperation = SceneManager.LoadSceneAsync(name);
+        asyncOperation.allowSceneActivation = false;
+        while (timer < loadingDelayTime)
+        {
+            timer += Time.deltaTime;
+            float fillAmount = Mathf.Clamp01(timer / loadingDelayTime);
+            UpdateLoadingUI(fillAmount);
+            yield return null;
+        }
+        yield return new WaitForSeconds(1f);
+
+        asyncOperation.allowSceneActivation = true;
+
+    }
+    
+    public void OpenGithubLink()
+    {
+
+        Application.OpenURL("https://github.com/qasimmu/2DPlatformerGame.git");
+    }
+    
+        public void OpenItchio()
+    {
+
+        Application.OpenURL("https://rainbow-flamingo.itch.io/jumpquest");
+    }
+    
+
+}
